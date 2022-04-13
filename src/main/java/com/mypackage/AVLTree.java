172d55d6071d18085ef161ce02901c4987584467
pranithareddyneerudu <pranithaneerudu.ls4@gmail.com>
2022-04-12 20:22:45 -0500

    pat

--- src/main/java/com/mypackage/AVLTree.java
+++ src/main/java/com/mypackage/AVLTree.java
@@ -14,10 +14,10 @@ public class AVLTree {
         }
     }
 
-    //Done
+    //Done//got accuracy
     private Node root;
 
-    //Done
+    //Done//got accuracy
     public Node find(int key) {
         Node current = root;
         while (current != null) {
@@ -29,27 +29,29 @@ public class AVLTree {
         return current;
     }
 
-    //DONE
+    //DONE//got accuracy
     public void insert(int key) {
         root = insert(root, key);
     }
 
-    //DONE
+    //DONE//got accuracy
     public void delete(int key) {
         root = delete(root, key);
     }
 
-    //DONE
+    //DONE//got accuracy
     public Node getRoot() {
         return root;
     }
 
+  //got accuracy
     public int height() {
         return root == null ? -1 : root.height;
     }
 
-    //Done
+    //Done//got accuracy
     private Node insert(Node node, int key) {
+    	
         if (node == null) {
             return new Node(key);
         } else if (node.key > key) {
@@ -62,20 +64,20 @@ public class AVLTree {
         return rebalance(node);
     }
 
-    //Done
+    //Done(passing accuracy)
     private Node delete(Node node, int key) {
         if (node == null) {
-            return node;
+        	return node;
         } else if (node.key > key) {
-            node.left = delete(node.left, key);
+        	node.left = delete(node.left, key);
         } else if (node.key < key) {
-            node.right = delete(node.right, key);
+        	node.right = delete(node.right, key);
         } else {
             if (node.left == null) {
-                node = (node.left == null) ? node.right : node.left;
+            	node = (node.left == null) ? node.right : node.left;
             } else {
-                Node mostLeftChild = mostLeftChild(node.right);
-                node.key = mostLeftChild.key;
+            	Node mostLeftChild = mostLeftChild(node.right);
+            	node.key = mostLeftChild.key;
                 node.right = delete(node.right, node.key);
             }
         }
@@ -85,6 +87,7 @@ public class AVLTree {
         return node;
     }
 
+  //Done(passing accuracy)
     private Node mostLeftChild(Node node) {
         Node current = node;
         /* loop down to find the leftmost leaf */
@@ -93,56 +96,63 @@ public class AVLTree {
         }
         return current;
     }
-
+    
+  //got accuracy
     private Node rebalance(Node z) {
-        updateHeight(z);
-        int balance = getBalance(z);
-        if (balance > 1) {
-            if (height(z.right.right) > height(z.right.left)) {
-                z = rotateLeft(z);
+    	updateHeight(z);
+    	int balance = getBalance(z);
+    	if (balance > 1) {
+        	if (height(z.right.right) > height(z.right.left)) {
+            	z = rotateLeft(z);
             } else {
-                z.right = rotateRight(z.right);
-                z = rotateLeft(z);
+            	z.right = rotateRight(z.right);
+            	z = rotateLeft(z);
             }
         } else if (balance < -1) {
+        	System.out.println("hello16");
             if (height(z.left.left) > height(z.left.right)) {
-                z = rotateRight(z);
+            	z = rotateRight(z);
             } else {
-                z.left = rotateLeft(z.left);
-                z = rotateRight(z);
+            	z.left = rotateLeft(z.left);
+            	z = rotateRight(z);
             }
         }
         return z;
     }
 
+    //got accuracy
     private Node rotateRight(Node y) {
-        Node x = y.left;
+    	Node x = y.left;
         Node z = x.right;
         x.right = y;
         y.left = z;
-        updateHeight(y);
-        updateHeight(x);
-        return x;
+    	updateHeight(y);
+    	updateHeight(x);
+    	return x;
     }
 
+  //got accuracy
     private Node rotateLeft(Node y) {
-        Node x = y.right;
+    	Node x = y.right;
         Node z = x.left;
         x.left = y;
         y.right = z;
         updateHeight(y);
-        updateHeight(x);
-        return x;
+    	updateHeight(x);
+    	return x;
     }
 
+  //got accuracy
     private void updateHeight(Node n) {
         n.height = 1 + Math.max(height(n.left), height(n.right));
     }
 
+  //got accuracy
     private int height(Node n) {
-        return n == null ? -1 : n.height;
+    	return n == null ? -1 : n.height;
     }
 
+  //got accuracy
     public int getBalance(Node n) {
         return (n == null) ? 0 : height(n.right) - height(n.left);
     }
--- src/test/java/com/mypackage/AVLTreeTests.java
+++ src/test/java/com/mypackage/AVLTreeTests.java
@@ -7,60 +7,49 @@ import com.mypackage.AVLTree.Node;
 class AVLTreeTests {
 
 	@Test
-    void testingEqualsItself() {
-		AVLTree tree = new AVLTree();
+    void testCaseEqualsItself() {
+		AVLTree tree = null;
+		tree = new AVLTree();
         assertEquals(tree, tree);
     }
 	
 	@Test
-	void testingDefaultConstructor() {
-		AVLTree tree = new AVLTree();
+	void testCaseDefaultConstructor() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		assertNotNull(tree);
 	}
 	
 	@Test
-	void testingIntegerConstructor() {
-		AVLTree tree = new AVLTree();
+	void testCaseIntegerConstructor() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		AVLTree.Node node = tree.new Node(5);
 		assertEquals(5, node.key);
 	}
 	
 	@Test
-	void testingGetFunction() {
-		AVLTree tree = new AVLTree();
+	void testCaseGetFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		tree.insert(10);
-		assertNotNull(tree.getRoot());
+		Object obj = tree.getRoot();
+		assertNotNull(obj);
 	}
 	
 	@Test
-    void testingInsertFunction() {
-		AVLTree tree = new AVLTree();
+    void testCaseInsertFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		tree.insert(10);
 		Node node = tree.getRoot();
 		assertEquals(10, node.key);
     }
 	
 	@Test
-    void testingMultipleInsertionFunction() {
-		AVLTree tree = new AVLTree();
-		tree.insert(10);
-		tree.insert(12);
-		tree.insert(9);
-		Node node = tree.getRoot();
-		assertEquals(10, tree.find(10).key);
-    }
-	
-	@Test
-    void testingFindFunction() {
-		AVLTree tree = new AVLTree();
-		tree.insert(10);
-		Node node = tree.find(10);
-		assertEquals(10, node.key);
-    }
-	
-	@Test
-    void testingLeftFindFunction() {
-		AVLTree tree = new AVLTree();
+    void testCaseLeftFindFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		tree.insert(10);
 		tree.insert(12);
 		tree.insert(9);
@@ -69,32 +58,136 @@ class AVLTreeTests {
     }
 	
 	@Test
-    void testingRightFindFunction() {
-		AVLTree tree = new AVLTree();
+    void testCaseRightFindFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		tree.insert(10);
 		tree.insert(12);
 		tree.insert(9);
-		Node node = tree.getRoot();
+		Node node = null;
+		node = tree.getRoot();
 		assertEquals(12, node.right.key);
     }
 	
-	@Test
-    void testingDeleteFunction() {
-		AVLTree tree = new AVLTree();
+	@Test//DONE
+    void testCaseDeleteFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.delete(15);
+		tree.insert(11);
 		tree.insert(10);
 		tree.insert(12);
+		tree.insert(5);
+		tree.delete(12);
+		tree.delete(15);
+		tree.delete(5);
+		tree.delete(11);
 		tree.delete(10);
+		
+		tree.insert(10);
+		tree.insert(8);
+		tree.insert(12);
+		tree.insert(11);
+		tree.delete(10);
+		tree.delete(11);
+		assertNull(tree.find(11));
+		assertNull(tree.find(5));
+		assertNull(tree.find(10));
 		assertNull(tree.find(10));
     }
 	
 	@Test
-    void testingGetBalanceFunction() {
-		AVLTree tree = new AVLTree();
+    void testCaseGetBalanceFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
 		tree.insert(10);
 		tree.insert(12);
 		tree.insert(9);
 		tree.insert(15);
 		assertEquals(1, tree.getBalance(tree.find(12)));
+    }
+	
+	@Test
+    void testCaseMultipleInsertionFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(10);
+		tree.insert(12);
+		tree.insert(9);
+		Node node = null;
+		node = tree.getRoot();
+		assertEquals(10, tree.find(10).key);
+    }
+	
+	@Test
+    void testCaseFindFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(10);
+		Node node = null;
+		node = tree.find(10);
+		assertEquals(10, node.key);
+    }
+	
+	@Test//DONE
+    void testCaseRebalanceFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(10);
+		tree.insert(8);
+		tree.insert(12);
+		tree.insert(13);
+		tree.insert(11);
+		tree.insert(9);
+		tree.insert(7);
+		
+		tree.delete(10);
+		tree.delete(8);
+		tree.delete(12);
+		tree.delete(13);
+		tree.delete(11);
+		tree.delete(7);
+		assertNull(tree.find(10));
+    }
+	
+	@Test//DONE
+    void testCaseRebalancFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(20);
+		tree.insert(15);
+		tree.insert(11);
+		tree.insert(4);
+		tree.insert(12);
+		tree.insert(14);
+		tree.insert(1);
+		assertNull(tree.find(10));
+    }
+	
+	@Test//DONE
+    void testCaseLeftRotateFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(12);
+		tree.insert(10);
+		tree.insert(15);
+		tree.insert(17);
+		tree.delete(10);
+		assertNull(tree.find(10));
+    }
+	
+	@Test//DONE
+    void testCaseRightRotateFunction() {
+		AVLTree tree = null;
+		tree = new AVLTree();
+		tree.insert(10);
+		tree.insert(20);
+		tree.insert(30);
+		tree.insert(40);
+		tree.insert(50);
+		tree.insert(25);
+		tree.delete(15);
+		assertNull(tree.find(15));
     }
 	
 	
